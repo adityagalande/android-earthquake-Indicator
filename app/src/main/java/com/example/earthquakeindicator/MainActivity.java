24f37ba730a2d00a978fcc05db583c1ceda2bfb7
Aditya Galande <adityagalande14@gmail.com>
2020-12-16 18:03:50 +0530

    JSONObject instantiated in mainActivity.java

--- app/src/main/java/com/example/earthquakeindicator/MainActivity.java
+++ app/src/main/java/com/example/earthquakeindicator/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.earthquakeindicator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,13 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<earthquakeData> earthquakes = new ArrayList<>();
-        earthquakes.add(new earthquakeData( "1.2","Delhi","Nov 12, 2019"));
-        earthquakes.add(new earthquakeData( "5.3","Washington DC","Jan 16, 2020"));
-        earthquakes.add(new earthquakeData( "3.7","Ludhiyana","Dec 27, 1979"));
-        earthquakes.add(new earthquakeData( "5.5","Toronto","Aug 30, 1870"));
-        earthquakes.add(new earthquakeData( "9.7","Hiroshima","Feb 17, 1767"));
-        earthquakes.add(new earthquakeData( "7.7","Hawana","Sept 03, 1763"));
+//        ArrayList<earthquakeData> earthquakes = new ArrayList<>();
+//        earthquakes.add(new earthquakeData( "1.2","Delhi","Nov 12, 2019"));
+//        earthquakes.add(new earthquakeData( "5.3","Washington DC","Jan 16, 2020"));
+//        earthquakes.add(new earthquakeData( "3.7","Ludhiyana","Dec 27, 1979"));
+//        earthquakes.add(new earthquakeData( "5.5","Toronto","Aug 30, 1870"));
+//        earthquakes.add(new earthquakeData( "9.7","Hiroshima","Feb 17, 1767"));
+//        earthquakes.add(new earthquakeData( "7.7","Hawana","Sept 03, 1763"));
+
+        ArrayList<earthquakeData> earthquakes = QueryUtils.extractEarthquakes();
 
         ListView listView = (ListView) findViewById(R.id.earthquakeList);
 
